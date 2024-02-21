0972a0ce20cdb1b5015c44a38c8c67396d0e8d52
A-M <a.hassan7041@su.edu.eg>
2024-02-21 18:28:39 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
