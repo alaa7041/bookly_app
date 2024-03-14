dad885bf2d83e38f07d518e8b87e901eb72e5d2b
A-M <a.hassan7041@su.edu.eg>
2024-03-14 14:25:18 +0200

    created failures

---
+++ lib/features/home/data/models/book_model/epub.dart
@@ -0,0 +1,20 @@
+
+class Epub  {
+  final bool? isAvailable;
+  final String? acsTokenLink;
+
+  const Epub({this.isAvailable, this.acsTokenLink});
+
+  factory Epub.fromJson(Map<String, dynamic> json) => Epub(
+        isAvailable: json['isAvailable'] as bool?,
+        acsTokenLink: json['acsTokenLink'] as String?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'isAvailable': isAvailable,
+        'acsTokenLink': acsTokenLink,
+      };
+
+  @override
+  List<Object?> get props => [isAvailable, acsTokenLink];
+}
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+import '../models/book_model/book_model.dart';
+
 abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchBeastSellerBooks();
 
-   fetchBeastSellerBooks();
-  fetchFeaturedBooks();
-
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
