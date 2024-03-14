4d865602cbe9b2b5b81bac7404af0caabf2fe5f8
A-M <a.hassan7041@su.edu.eg>
2024-03-14 17:36:14 +0200

    created ApI service

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo{
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBeastSellerBooks() {
+    // TODO: implement fetchBeastSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
